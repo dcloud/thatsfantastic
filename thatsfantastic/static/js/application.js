7a52e4e3404fcba4ca1680ae385131287b447a5e
Daniel Cloud <daniel@danielcloud.org>
2015-09-10 15:53:49 -0400

    Improve tabs functionality.

--- thatsfantastic/static/js/application.js
+++ thatsfantastic/static/js/application.js
@@ -17,21 +17,29 @@ jQuery(document).ready(function($) {
         var tabs = $('.tab-sections > section');
         tabs.hide();
 
-        tabButtons.on('click', function(event) {
+        tabButtons.each(function(index, el) {
+            var href = $(el).attr('href');
+            var enabled = false;
+            if (href.charAt(0) == "#") {
+                $(el).data('href', href);
+                enabled = true;
+            }
+            $(el).data('tab-enabled', enabled);
+        }).on('click', function(event) {
+            event.preventDefault();
             var tabButton = $(this);
             tabButton.addClass('active');
             tabButtons.not(tabButton).removeClass('active');
-            var href = tabButton.attr('href');
-            if (href.charAt(0) == "#") {
-                tabs.not(href).hide();
-                $(href).show();
+            var dhref = tabButton.data('href');
+            if (dhref.charAt(0) == "#") {
+                tabs.not(dhref).hide();
+                $(dhref).show();
             }
         });
 
         if (!document.location.hash) {
             tabs.first().show();
             tabButtons.first().addClass('active');
-            document.location.hash = tabs.first().attr('id');
         } else {
             tabButtons.filter("[href=" + document.location.hash + "]").click();
         }

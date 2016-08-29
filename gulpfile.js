8998fd8891e7f991d29f013ba2cbc1ee2e694047
Daniel Cloud <daniel@danielcloud.org>
2016-08-28 21:38:48 -0400

    Update gulp build system for sass. I might have overonfigured browsersync.

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,41 @@
+'use strict';
+
 var gulp = require('gulp'),
-    streamqueue = require('streamqueue'),
     concat = require('gulp-concat'),
-    minifyCSS = require('gulp-minify-css'),
-    sass = require('gulp-ruby-sass'),
+    cleanCSS = require('gulp-clean-css'),
+    sass = require('gulp-sass'),
+    sourcemaps  = require('gulp-sourcemaps'),
     rename = require('gulp-rename'),
-    browserSync = require('browser-sync'),
-    reload = browserSync.reload;
+    browserSync = require('browser-sync').create();
 
 var staticDir = 'thatsfantastic/static/',
     cssDir = staticDir + 'css/',
-    sassDir = staticDir + 'sass/';
+    sassDir = staticDir + 'sass/',
+    sassGlob = '/**/*.scss';
 
 gulp.task('sass', function() {
-    return sass(sassDir)
-    .on('error', sass.logError)
-    .pipe(concat('main.css'))
-    .pipe(gulp.dest(cssDir))
-    .pipe(minifyCSS())
-    .pipe(rename({ extname: '.min.css' }))
-    .pipe(gulp.dest(cssDir))
+  return gulp.src(sassDir + sassGlob)
+             .pipe(sourcemaps.init())
+             .pipe(sass().on('error', sass.logError))
+             .pipe(cleanCSS({processImportFrom: ['local']}))
+             .pipe(concat('main.min.css'))
+             .pipe(sourcemaps.write('./maps'))
+             .pipe(gulp.dest(cssDir))
+             .pipe(browserSync.stream());
 });
 
 gulp.task('build', ['sass'])
 
 // watch files for changes and reload
-gulp.task('watch', function() {
-  gulp.watch(sassDir + '*.scss', ['sass', reload]);
+gulp.task('serve', ['sass'], function() {
+  gulp.watch(sassDir + sassGlob, ['sass']);
+  browserSync.init({
+    proxy: "localhost:8000",
+    serveStatic: [staticDir],
+    files: [cssDir + '*.css'],
+    open: false
+  });
+
 });
 
 gulp.task('default', ['build'])

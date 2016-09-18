8cbe95a89fa29f9cc1a4f75231d88e681dfe4892
Daniel Cloud <daniel@danielcloud.org>
2016-09-18 16:38:35 -0400

    Correct the order of whitenoise.runserver_nostatic in INSTALLED_APPS

--- thatsfantastic/settings.py
+++ thatsfantastic/settings.py
@@ -27,13 +27,12 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'whitenoise.runserver_nostatic',
     'django.contrib.staticfiles',
 
     'scraper',
     'cinema',
     'fantasticfest',
-
-    'whitenoise.runserver_nostatic',
 ]
 
 MIDDLEWARE = [

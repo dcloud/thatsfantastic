5b806f22ff85fd9c0a290958a4f4599542b58f98
Daniel Cloud <daniel@danielcloud.org>
2019-05-27 23:16:48 -0400

    Update urls to new path and path_re handlers

--- cinema/urls.py
+++ cinema/urls.py
@@ -1,20 +1,33 @@
-from django.conf.urls import url
+from django.urls import path, re_path
 from django.conf import settings
-from cinema.views import (FilmDetail, FilmSearch, FilmList,
-                          CountryFilmList, CountryList,
-                          EventDetail, EventCountryFilmList)
+from cinema.views import (
+    FilmDetail,
+    FilmSearch,
+    FilmList,
+    CountryFilmList,
+    CountryList,
+    EventDetail,
+    EventCountryFilmList,
+)
 
-CINEMA_DEFAULT_EVENT = getattr(settings, 'CINEMA_DEFAULT_EVENT', None)
+CINEMA_DEFAULT_EVENT = getattr(settings, "CINEMA_DEFAULT_EVENT", None)
 
-app_name = 'cinema'
+app_name = "cinema"
 urlpatterns = [
-    url(r'^events/(?P<slug>[\w\-]+)/$', EventDetail.as_view(), name='film-event-detail'),
-    url(r'^events/(?P<event_slug>[\w\-]+)/country/(?P<country_slug>[\w\-\.]+)/$',
-        EventCountryFilmList.as_view(), name='films-event-country'),
-    url(r'^search/films/$', FilmSearch.as_view(), name='films-search'),
-    url(r'^films/$', FilmList.as_view(), name='all-films-list'),
-    url(r'^films/(?P<slug>[\w\-\.]+)/$', FilmDetail.as_view(), name='film-detail'),
-    url(r'^countries/(?P<slug>[\w\-\.]+)/$', CountryFilmList.as_view(), name='films-from-country'),
-    url(r'^countries/$', CountryList.as_view(), name='countries-from-films'),
-    url(r'^$', EventDetail.as_view(), {'slug': CINEMA_DEFAULT_EVENT}, name='films-list'),
+    path("events/<slug:slug>/", EventDetail.as_view(), name="film-event-detail"),
+    path(
+        "events/<slug:title>/country/<slug:country>/",
+        EventCountryFilmList.as_view(),
+        name="films-event-country",
+    ),
+    path("search/films/", FilmSearch.as_view(), name="films-search"),
+    path("films/", FilmList.as_view(), name="all-films-list"),
+    re_path(r"^films/(?P<slug>[\w\-\.]+)/$", FilmDetail.as_view(), name="film-detail"),
+    re_path(
+        r"^countries/(?P<slug>[\w\-\.]+)/$",
+        CountryFilmList.as_view(),
+        name="films-from-country",
+    ),
+    path("countries/", CountryList.as_view(), name="countries-from-films"),
+    path("", EventDetail.as_view(), {"slug": CINEMA_DEFAULT_EVENT}, name="films-list"),
 ]
--- thatsfantastic/urls.py
+++ thatsfantastic/urls.py
@@ -1,13 +1,13 @@
 from django.conf import settings
-from django.conf.urls import include, url
 from django.conf.urls.static import static
+from django.urls import include, path
 from django.contrib import admin
 
-urlpatterns = [url(r"^", include("cinema.urls")), url(r"^admin/", admin.site.urls)]
+urlpatterns = [path("", include("cinema.urls")), path("admin/", admin.site.urls)]
 
 
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     import debug_toolbar
 
-    urlpatterns += [url(r"^__debug__/", include(debug_toolbar.urls))]
+    urlpatterns += [path("__debug__/", include(debug_toolbar.urls))]

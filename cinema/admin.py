fc0d27adf892130d9deb7124f22ba88705bb256d
Daniel Cloud <daniel@danielcloud.org>
2019-06-02 14:51:24 -0400

    Add DecadeListFilter

--- cinema/admin.py
+++ cinema/admin.py
@@ -1,5 +1,27 @@
 from django.contrib import admin
+from django.utils.translation import ugettext as _
 from cinema.models import Film, Person, Screening, Event, Country
+from datetime import datetime
+
+
+class DecadeListFilter(admin.SimpleListFilter):
+    title = _("Decade")
+
+    parameter_name = "decade"
+
+    def lookups(self, request, model_admin):
+        today = datetime.today()
+        decades = range(1900, today.year + 1, 10)
+        return ((f"{d}s", f"{d}s") for d in decades)
+
+    def queryset(self, request, queryset):
+        decade_value = self.value()[:-1] if self.value() else None
+        if decade_value:
+            try:
+                decade = int(decade_value)
+                return queryset.filter(year__gte=decade, year__lt=decade + 10)
+            except Exception:
+                return queryset.none()
 
 
 @admin.register(Film)
@@ -7,7 +29,7 @@ class FilmAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("title",)}
     list_display = ("title", "runtime", "year", "slug")
     search_fields = ("title", "description")
-    list_filter = ["year", "countries", "shown_at__title"]
+    list_filter = [DecadeListFilter, "countries", "shown_at__title"]
     filter_horizontal = ["directors", "actors", "countries"]
 
 
@@ -17,6 +39,7 @@ class EventAdmin(admin.ModelAdmin):
     list_display = ("title", "start_day", "end_day", "location")
     search_fields = ("title", "location")
     filter_horizontal = ("films",)
+    date_hierarchy = "start_day"
 
 
 @admin.register(Country)

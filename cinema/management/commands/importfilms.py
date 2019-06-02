57a2b81b36c6db1087d7e72d5bf6b050ec56275b
Daniel Cloud <daniel@danielcloud.org>
2019-06-02 01:57:18 -0400

    Update syntax for setting m2m relationships in Django 2.x

--- cinema/management/commands/importfilms.py
+++ cinema/management/commands/importfilms.py
@@ -3,7 +3,7 @@ import os.path
 from os import listdir
 import json
 from nameparser import HumanName
-from cinema.models import (Film, Person, Event, Country)
+from cinema.models import Film, Person, Event, Country
 from cinema.utils import titlecase
 
 
@@ -11,26 +11,31 @@ class Command(BaseCommand):
     """Load film data from a directory path or file."""
 
     def add_arguments(self, parser):
-        parser.add_argument('path',
-                            help='A path to a directory of JSON files or \
-                                  single file with film data.')
-        parser.add_argument('--event', nargs='?',
-                            help='Slug for an event to associate this film with.')
-        parser.add_argument('-n', '--dry-run', action='store_true', dest='dryrun')
+        parser.add_argument(
+            "path",
+            help="A path to a directory of JSON files or \
+                                  single file with film data.",
+        )
+        parser.add_argument(
+            "--event", nargs="?", help="Slug for an event to associate this film with."
+        )
+        parser.add_argument("-n", "--dry-run", action="store_true", dest="dryrun")
 
     def handle(self, *args, **options):
-        self.verbosity = options['verbosity']
-        self.dryrun = options['dryrun']
-        self.event = options.get('event', None)
+        self.verbosity = options["verbosity"]
+        self.dryrun = options["dryrun"]
+        self.event = options.get("event", None)
         if self.dryrun:
             self.stdout.write("Dry run. Nothing will be created in the database.")
-        if os.path.exists(options['path']):
-            path = os.path.abspath(options['path'])
+        if os.path.exists(options["path"]):
+            path = os.path.abspath(options["path"])
             json_files = []
             if os.path.isdir(path):
                 if self.verbosity > 0:
                     self.stdout.write("Examining JSON files at {}".format(path))
-                json_files = [os.path.join(path, f) for f in listdir(path) if f.endswith('.json')]
+                json_files = [
+                    os.path.join(path, f) for f in listdir(path) if f.endswith(".json")
+                ]
             elif os.path.isfile(path):
                 json_files = [path]
             for fpath in json_files:
@@ -40,32 +45,31 @@ class Command(BaseCommand):
                 if self.verbosity > 2:
                     self.stdout.write(json.dumps(data))
         else:
-            raise CommandError('Path is not a valid path')
+            raise CommandError("Path is not a valid path")
 
     def _create_film_entry(self, data, path):
         if isinstance(data, dict):
             (head, tail) = os.path.split(path)
             (name, ext) = os.path.splitext(tail)
-            title = data.get('title', None)
-            directors = self._process_directors(data.get('directors', []))
+            title = data.get("title", None)
+            directors = self._process_directors(data.get("directors", []))
             if not title:
                 raise CommandError("Film data has no title!")
-            (instance, created) = Film.objects\
-                .get_or_create(slug=name,
-                               year=data.get('year', None),
-                               )
+            (instance, created) = Film.objects.get_or_create(
+                slug=name, year=data.get("year", None)
+            )
             instance.title = titlecase(title)
-            instance.synopsis = data.get('synopsis', instance.synopsis)
-            instance.description = data.get('description', instance.description)
-            instance.runtime = data.get('runtime', instance.runtime)
-            for country_name in set(data.get('countries', [])):
+            instance.synopsis = data.get("synopsis", instance.synopsis)
+            instance.description = data.get("description", instance.description)
+            instance.runtime = data.get("runtime", instance.runtime)
+            for country_name in set(data.get("countries", [])):
                 (country, _) = Country.objects.get_or_create(name=country_name)
                 instance.countries.add(country)
 
-            instance.directors = directors
-            has_source_url = ('meta' in data and 'source_url' in data['meta'])
+            instance.directors.set(directors)
+            has_source_url = "meta" in data and "source_url" in data["meta"]
             if has_source_url:
-                source_url = data['meta']['source_url']
+                source_url = data["meta"]["source_url"]
                 url_set = set(instance.related_urls)
                 url_set.add(source_url)
                 instance.related_urls = list(url_set)
@@ -75,10 +79,20 @@ class Command(BaseCommand):
                     event = Event.objects.get(slug=self.event)
                     event.films.add(instance)
                     if self.verbosity > 2:
-                        self.stdout.write("Associated '{}' with '{}'".format(instance.title, event.title))
+                        self.stdout.write(
+                            "Associated '{}' with '{}'".format(
+                                instance.title, event.title
+                            )
+                        )
                 except Event.DoesNotExist:
-                    self.stderr.write("Record does not exist for slug '{}'".format(self.event))
-            self.stdout.write("{} film '{}'".format('Created' if created else 'Updated', str(instance)))
+                    self.stderr.write(
+                        "Record does not exist for slug '{}'".format(self.event)
+                    )
+            self.stdout.write(
+                "{} film '{}'".format(
+                    "Created" if created else "Updated", str(instance)
+                )
+            )
         else:
             self.stderr.write("Error reading film data from a non-dictionary instance")
 
@@ -86,7 +100,7 @@ class Command(BaseCommand):
         data = None
         if self.verbosity > 1:
             self.stdout.write("Loading '{}'".format(fpath))
-        with open(fpath, 'r') as fp:
+        with open(fpath, "r") as fp:
             try:
                 data = json.load(fp)
             except ValueError:
@@ -97,10 +111,14 @@ class Command(BaseCommand):
         people = []
         for name_str in directors_list:
             name = HumanName(name_str)
-            (instance, created) = Person.objects.get_or_create(first_name=name.first,
-                                                             middle_name=name.middle,
-                                                             last_name=name.last)
+            (instance, created) = Person.objects.get_or_create(
+                first_name=name.first, middle_name=name.middle, last_name=name.last
+            )
             if self.verbosity > 1:
-                self.stdout.write("{} person '{}'".format('Created' if created else 'Updated', str(instance)))
+                self.stdout.write(
+                    "{} person '{}'".format(
+                        "Created" if created else "Updated", str(instance)
+                    )
+                )
             people.append(instance)
         return people
